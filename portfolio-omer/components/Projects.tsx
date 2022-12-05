26abcc8f1025687f968df9afeac4e7c34bfd80c4
Omer Degirmenci <59254278+DegirmenciOmer@users.noreply.github.com>
2022-12-05 12:54:36 +0100

    Develop (#12)
    * add experience, add contact info
    
    * fix header alignment
    
    * header smaller in mobile mode
    
    * add footer, add contact links
    
    * sort experience and projects
    
    * fix typescript warning
    
    * Header Component
    
    * Fix description, Add Showcase, Experience, Header, Projects, Skills, Footer components
    
    * fix projects with empty links
    
    * edit README
    
    * build pages
    
    * Edit content data, add description for each project
    
    * add context
    
    * handle  projects routing and internationalization
    
    * fix routes
    
    * edit project links according to their translation match
    
    * edit link
    
    * cancel i18n translatios
    
    * edit projects pages
    
    * fix key prop
    
    * edit projects, add images
    
    * edit projects image

--- portfolio-omer/components/Projects.tsx
+++ portfolio-omer/components/Projects.tsx
@@ -13,45 +13,27 @@ const Projects: FC<TProjectsProps> = ({ switchText }) => {
   return (
     <section
       id="projects"
-      className={`shadow-lg p-10 rounded-xl my-10 flex-1 ${switchText}`}
+      className={`shadow-lg p-10 my-10 flex-1 ${switchText}`}
     >
       <h3 className="text-center py-4 text-2xl mb-5 text-teal-600">
         {translations.projectsTitle}
       </h3>
-      <ul>
+      <ul className="flex flex-col gap-4 items-center justify-center">
         {translations.projects
           .sort((a, b) => +b.id - +a.id)
           .map((project) => (
             <li
               key={project.id}
-              className={`${switchText} flex justify-between max-w-xl mx-auto gap-1 my-4`}
+              className={`${switchText} rounded-lg p-3 flex flex-col items-center max-w-xl mx-auto gap-1 my-4`}
             >
-              <Link
-                className="hover:text-teal-600"
-                href={`projects/${+project.id}`}
-              >
-                {project.name}
+              <Link href={`projects/${+project.id}`}>
+                <img
+                  className="mx-auto rounded-lg w-full my-5"
+                  src={project.img}
+                  alt="profile"
+                />
+                <p className="text-center">{project.name}</p>
               </Link>
-              <div className="align-left">
-                <Link
-                  className={`text-teal-800 underline hover:text-teal-600 rounded-md `}
-                  href={project.liveUrl ? project.liveUrl : project.demoUrl}
-                >
-                  {project.liveUrl
-                    ? translations.liveText
-                    : project.demoUrl
-                    ? translations.demoText
-                    : ""}
-                </Link>
-                {project.sourceUrl && (
-                  <Link
-                    className={`text-teal-800 underline hover:text-teal-600 rounded-md`}
-                    href={project.sourceUrl}
-                  >
-                    &nbsp;|&nbsp;{translations.sourceText}
-                  </Link>
-                )}
-              </div>
             </li>
           ))}
       </ul>
---
+++ portfolio-omer/pages/contact.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className={`md:px-20 lg:px-40 text-blue-100 font-bold  `}>contact</div>
+  );
+};
+
+export default Contact;
