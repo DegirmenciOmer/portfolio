00f604dfafe62fa4bc516dbc63f2452a69db4b87
DegirmenciOmer <rumidegirmenci0@gmail.com>
2022-12-05 23:31:14 +0100

    fix minor error

--- portfolio-omer/pages/projects/[id].tsx
+++ portfolio-omer/pages/projects/[id].tsx
@@ -136,7 +136,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
 export const getStaticPaths: GetStaticPaths = async () => {
   const paths = projects.map((project) => {
     return {
-      params: { id: id.toString() },
+      params: { id: project.toString() },
     };
   });
 
