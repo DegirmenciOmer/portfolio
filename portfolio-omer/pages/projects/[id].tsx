7f5c0aac658924ff0fce09927ae0defbe0581328
Omer Degirmenci <59254278+DegirmenciOmer@users.noreply.github.com>
2022-12-05 13:49:43 +0100

    Develop (#13)
    * add experience, add contact info
    
    * fix header alignment
    
    * header smaller in mobile mode
    
    * add footer, add contact links
    
    * sort experience and projects
    
    * fix typescript warning
    
    * Header Component
    
    * Fix description, Add Showcase, Experience, Header, Projects, Skills, Footer components
    
    * fix projects with empty links
    
    * edit README
    
    * build pages
    
    * Edit content data, add description for each project
    
    * add context
    
    * handle  projects routing and internationalization
    
    * fix routes
    
    * edit project links according to their translation match
    
    * edit link
    
    * cancel i18n translatios
    
    * edit projects pages
    
    * fix key prop
    
    * edit projects, add images
    
    * edit projects image
    
    * add pagination
    
    * fix pagination direction

--- portfolio-omer/pages/projects/[id].tsx
+++ portfolio-omer/pages/projects/[id].tsx
@@ -5,7 +5,10 @@ import React, { FC } from "react";
 import { useAppContext } from "../../context/AppContext";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import { BsFillArrowLeftCircleFill } from "react-icons/bs";
+import {
+  BsFillArrowLeftCircleFill,
+  BsFillArrowRightCircleFill,
+} from "react-icons/bs";
 import Skills, { skill } from "../../components/Skills";
 
 const {
@@ -32,26 +35,26 @@ const DynamicPage: FC<TProject> = ({ project }) => {
   const switchBg = darkMode ? "bg-slate-800" : "bg-slate-200";
   const switchText = darkMode ? "text-slate-100" : "text-slate-800";
 
-  const translatedProject = projects[+project.id];
+  const projectId = +project.id;
 
   return (
     <div className={`md:px-20 lg:px-40 text-blue-100 font-bold ${switchBg} `}>
       <Header switchText={switchText} switchBg={switchBg} />
       <main className={`${switchText} px-5 min-h-screen max-w-3xl mx-auto`}>
         <Link href="/#projects">
-          <button className="bg-teal-500 my-4 p-2 px-3 rounded-md text-2xl">
+          <button className="text-teal-500 my-4 p-2 px-3 rounded-md text-4xl">
             <BsFillArrowLeftCircleFill />
           </button>
         </Link>
         <h2 className="py-4 text-center text-teal-600 text-2xl mb-5">
-          {translatedProject.name}{" "}
+          {project.name}{" "}
         </h2>
         <img
           className="mx-auto rounded-lg w-full my-5"
           src={project.img}
           alt="profile"
         />
-        {translatedProject.description.map((text, idx) => (
+        {project.description.map((text, idx) => (
           <p key={idx} className="text-center">
             {text}
           </p>
@@ -83,6 +86,22 @@ const DynamicPage: FC<TProject> = ({ project }) => {
           skills={project.technologies}
           skillsTitle={translations.techTitle}
         />
+        <div className="flex gap-3 justify-center">
+          {projectId < projects.length - 1 && (
+            <Link href={`/projects/${projectId + 1}`}>
+              <button className="text-teal-500 my-4 p-2 px-3 rounded-md text-4xl">
+                <BsFillArrowLeftCircleFill />
+              </button>
+            </Link>
+          )}
+          {projectId > 0 && (
+            <Link href={`/projects/${projectId - 1}`}>
+              <button className="text-teal-500 my-4 p-2 px-3 rounded-md text-4xl">
+                <BsFillArrowRightCircleFill />
+              </button>
+            </Link>
+          )}
+        </div>
       </main>
       <Footer switchText={switchText} />
     </div>
