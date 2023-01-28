17cabdafe47b87b8366509b36785b8c18f5b537e
Omer Degirmenci <59254278+DegirmenciOmer@users.noreply.github.com>
2023-01-29 00:17:29 +0100

    Develop (#20)
    * add experience, add contact info
    
    * fix header alignment
    
    * header smaller in mobile mode
    
    * add footer, add contact links
    
    * sort experience and projects
    
    * fix typescript warning
    
    * Header Component
    
    * Fix description, Add Showcase, Experience, Header, Projects, Skills, Footer components
    
    * fix projects with empty links
    
    * edit README
    
    * build pages
    
    * Edit content data, add description for each project
    
    * add context
    
    * handle  projects routing and internationalization
    
    * fix routes
    
    * edit project links according to their translation match
    
    * edit link
    
    * cancel i18n translatios
    
    * edit projects pages
    
    * fix key prop
    
    * edit projects, add images
    
    * edit projects image
    
    * add pagination
    
    * fix pagination direction
    
    * edit header
    
    * add skills logos
    
    * add logos for project pages
    
    * fix linting error
    
    * fix skills alignment homepage
    
    * fix skill
    s on project pages
    
    * edit
    
    * refactor
    
    * fix minor error
    
    * fix minor error
    
    * add flags
     file
    
    * replace language text with flags
    
    * fix missing link on project pages
    
    * fix some images, add cursor pointer
    
    * Add events app
    
    * edit translationHelper
    
    * Add new project details rumi keuken

--- portfolio-omer/pages/index.tsx
+++ portfolio-omer/pages/index.tsx
@@ -1,19 +1,18 @@
-import Head from "next/head";
-import { useState } from "react";
-import { translationHelper } from "../data/translationHelper";
-import Header from "../components/Header";
-import Showcase from "../components/Showcase";
-import Skills from "../components/Skills";
-import Projects from "../components/Projects";
-import Experience from "../components/Experience";
-import Footer from "../components/Footer";
-import { useAppContext } from "../context/AppContext";
+import Head from 'next/head'
+import { translationHelper } from '../data/translationHelper'
+import Header from '../components/Header'
+import Showcase from '../components/Showcase'
+import Skills from '../components/Skills'
+import Projects from '../components/Projects'
+import Experience from '../components/Experience'
+import Footer from '../components/Footer'
+import { useAppContext } from '../context/AppContext'
 
 export default function Home() {
-  const { locale, darkMode } = useAppContext();
-  const switchBg = darkMode ? "bg-slate-800" : "bg-slate-200";
-  const switchText = darkMode ? "text-slate-100" : "text-slate-800";
-  const { translations } = translationHelper(locale);
+  const { locale, darkMode } = useAppContext()
+  const switchBg = darkMode ? 'bg-slate-800' : 'bg-slate-200'
+  const switchText = darkMode ? 'text-slate-100' : 'text-slate-800'
+  const { translations } = translationHelper(locale)
   return (
     <div>
       <Head>
@@ -21,12 +20,13 @@ export default function Home() {
       </Head>
       <div className={`md:px-20 lg:px-40 text-blue-100 font-bold ${switchBg} `}>
         <Header switchText={switchText} switchBg={switchBg} />
-        <main className="min-h-screen max-w-3xl mx-auto">
+        <main className='min-h-screen max-w-3xl mx-auto'>
           <Showcase switchText={switchText} />
           <Skills
             switchText={switchText}
             skills={translations.skills}
             skillsTitle={translations.skillsTitle}
+            isPage={false}
           />
           <Projects switchText={switchText} />
           <Experience switchText={switchText} />
@@ -34,5 +34,5 @@ export default function Home() {
         <Footer switchText={switchText} />
       </div>
     </div>
-  );
+  )
 }
