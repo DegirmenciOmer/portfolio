e3e2beb662438e74f9eeac7fda79e9604a11a2be
DegirmenciOmer <rumidegirmenci0@gmail.com>
2023-01-29 00:15:54 +0100

    Add new project details rumi keuken

--- portfolio-omer/components/Projects.tsx
+++ portfolio-omer/components/Projects.tsx
@@ -1,24 +1,24 @@
-import Link from "next/link";
-import React, { FC } from "react";
-import { useAppContext } from "../context/AppContext";
-import { translationHelper } from "../data/translationHelper";
+import Link from 'next/link'
+import React, { FC } from 'react'
+import { useAppContext } from '../context/AppContext'
+import { translationHelper } from '../data/translationHelper'
 
 interface TProjectsProps {
-  switchText: string;
+  switchText: string
 }
 const Projects: FC<TProjectsProps> = ({ switchText }) => {
-  const { locale } = useAppContext();
+  const { locale } = useAppContext()
 
-  const { translations } = translationHelper(locale);
+  const { translations } = translationHelper(locale)
   return (
     <section
-      id="projects"
+      id='projects'
       className={`shadow-lg p-10 my-10 flex-1 ${switchText}`}
     >
-      <h3 className="text-center py-4 text-2xl mb-5 text-teal-600">
+      <h3 className='text-center py-4 text-2xl mb-10 text-teal-600'>
         {translations.projectsTitle}
       </h3>
-      <ul className="flex flex-col gap-8 items-center justify-center">
+      <ul className='flex flex-col gap-8 items-center justify-center'>
         {translations.projects
           .sort((a, b) => +b.id - +a.id)
           .map((project) => (
@@ -26,14 +26,14 @@ const Projects: FC<TProjectsProps> = ({ switchText }) => {
               <li
                 className={`${switchText} mb-9 flex flex-col items-center align-center justify-center`}
               >
-                <img src={project.img} alt="profile" />
-                <p className="text-center mt-5">{project.name}</p>
+                <p className='text-center mb-9'>{project.name}</p>
+                <img src={project.img} alt='profile' />
               </li>
             </Link>
           ))}
       </ul>
     </section>
-  );
-};
+  )
+}
 
-export default Projects;
+export default Projects
