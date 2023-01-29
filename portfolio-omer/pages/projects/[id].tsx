9e45a1fc40ce80bbee4244a78e356f1e1f3d5c19
DegirmenciOmer <rumidegirmenci0@gmail.com>
2023-01-29 18:56:47 +0100

    Edit translationHelper

--- portfolio-omer/pages/projects/[id].tsx
+++ portfolio-omer/pages/projects/[id].tsx
@@ -1,32 +1,32 @@
-import { translationHelper } from "../../data/translationHelper";
-import { GetStaticPaths, GetStaticProps } from "next";
-import Link from "next/link";
-import React, { FC } from "react";
-import { useAppContext } from "../../context/AppContext";
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
+import { translationHelper } from '../../data/translationHelper'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import Link from 'next/link'
+import React, { FC } from 'react'
+import { useAppContext } from '../../context/AppContext'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
 import {
   BsFillArrowLeftCircleFill,
   BsFillArrowRightCircleFill,
-} from "react-icons/bs";
-import Skills, { skill } from "../../components/Skills";
+} from 'react-icons/bs'
+import Skills, { skill } from '../../components/Skills'
 
 const {
   translations: { projects },
-} = translationHelper("en");
+} = translationHelper('en')
 
 interface TProject {
   project: {
-    id: string;
-    name: string;
-    sourceUrl: string;
-    liveUrl: string;
-    technologies: skill[];
-    locale: string;
-    description: string[];
-    demoUrl: string;
-    img: string;
-  };
+    id: string
+    name: string
+    sourceUrl: string
+    liveUrl: string
+    technologies: skill[]
+    locale: string
+    description: string[]
+    demoUrl: string
+    img: string
+  }
 }
 
 const DynamicPage: FC<TProject> = ({
@@ -41,37 +41,37 @@ const DynamicPage: FC<TProject> = ({
     demoUrl,
   },
 }) => {
-  const { darkMode, locale } = useAppContext();
-  const { translations } = translationHelper(locale);
-  const switchBg = darkMode ? "bg-slate-800" : "bg-slate-200";
-  const switchText = darkMode ? "text-slate-100" : "text-slate-800";
+  const { darkMode, locale } = useAppContext()
+  const { translations } = translationHelper(locale)
+  const switchBg = darkMode ? 'bg-slate-800' : 'bg-slate-200'
+  const switchText = darkMode ? 'text-slate-100' : 'text-slate-800'
 
-  const projectId = +id;
+  const projectId = +id
 
   return (
     <div className={`md:px-20 lg:px-40 text-blue-100 font-bold ${switchBg} `}>
       <Header switchText={switchText} switchBg={switchBg} />
       <main className={`${switchText} px-5 min-h-screen max-w-3xl mx-auto`}>
-        <Link href="/#projects">
-          <button className="text-teal-500 my-4 p-2 px-3 rounded-md text-4xl">
+        <Link href='/#projects'>
+          <button className='text-teal-500 my-4 p-2 px-3 rounded-md text-4xl'>
             <BsFillArrowLeftCircleFill />
           </button>
         </Link>
-        <h2 className="py-4 text-center text-teal-600 text-2xl mb-5">
-          {name}{" "}
+        <h2 className='py-4 text-center text-teal-600 text-2xl mb-5'>
+          {name}{' '}
         </h2>
         <img
-          className="mx-auto rounded-lg w-full my-5"
+          className='mx-auto rounded-lg w-full my-5'
           src={img}
-          alt="profile"
+          alt='profile'
         />
         {description.map((text, idx) => (
-          <p key={idx} className="text-center">
+          <p key={idx} className='mb-3'>
             {text}
           </p>
         ))}
 
-        <div className="text-center mt-10">
+        <div className='text-center mt-10'>
           {(sourceUrl || demoUrl || liveUrl) && (
             <>
               {translations.seeText}&nbsp;
@@ -83,7 +83,7 @@ const DynamicPage: FC<TProject> = ({
                   ? translations.liveText
                   : demoUrl
                   ? translations.demoText
-                  : ""}
+                  : ''}
               </Link>
             </>
           )}
@@ -102,17 +102,17 @@ const DynamicPage: FC<TProject> = ({
           skillsTitle={translations.techTitle}
           isPage={true}
         />
-        <div className="flex gap-3 justify-center">
+        <div className='flex gap-3 justify-center'>
           {projectId < projects.length - 1 && (
             <Link href={`/projects/${projectId + 1}`}>
-              <button className="text-teal-500 my-4 p-2 px-3 rounded-md text-4xl">
+              <button className='text-teal-500 my-4 p-2 px-3 rounded-md text-4xl'>
                 <BsFillArrowLeftCircleFill />
               </button>
             </Link>
           )}
           {projectId > 0 && (
             <Link href={`/projects/${projectId - 1}`}>
-              <button className="text-teal-500 my-4 p-2 px-3 rounded-md text-4xl">
+              <button className='text-teal-500 my-4 p-2 px-3 rounded-md text-4xl'>
                 <BsFillArrowRightCircleFill />
               </button>
             </Link>
@@ -121,26 +121,26 @@ const DynamicPage: FC<TProject> = ({
       </main>
       <Footer switchText={switchText} />
     </div>
-  );
-};
+  )
+}
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const id = context?.params?.id;
-  const project = id ? projects[+id] : undefined;
+  const id = context?.params?.id
+  const project = id ? projects[+id] : undefined
 
   return {
     props: { project },
-  };
-};
+  }
+}
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const paths = projects.map((project) => {
     return {
       params: { id: project.id.toString() },
-    };
-  });
+    }
+  })
 
-  return { paths, fallback: false };
-};
+  return { paths, fallback: false }
+}
 
-export default DynamicPage;
+export default DynamicPage
