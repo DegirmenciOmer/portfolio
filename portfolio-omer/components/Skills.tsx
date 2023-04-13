855e05f61fd09fff8cc63c3fdcf75db989513647
DegirmenciOmer <rumidegirmenci0@gmail.com>
2023-04-13 16:17:24 +0200

    add vue tracker app

--- portfolio-omer/components/Skills.tsx
+++ portfolio-omer/components/Skills.tsx
@@ -1,14 +1,14 @@
-import Image from "next/image";
-import React, { FC } from "react";
+import React, { FC } from 'react'
+import { useAppContext } from '../context/AppContext'
 
 interface TSkillsProps {
-  switchText?: string;
-  skills: skill[];
-  skillsTitle: string;
-  isPage: boolean;
+  switchText?: string
+  skills: skill[]
+  skillsTitle: string
+  isPage: boolean
 }
 
-export type skill = { id: string; name: string; img: string };
+export type skill = { id: string; name: string; img: string }
 
 const Skills: FC<TSkillsProps> = ({
   switchText,
@@ -16,21 +16,24 @@ const Skills: FC<TSkillsProps> = ({
   skillsTitle,
   isPage,
 }) => {
+  const { darkMode } = useAppContext()
   return (
     <div
       className={`text-center shadow-lg py-10 rounded-xl my-10  flex-1 ${switchText}`}
     >
-      <h3 className="py-4 text-teal-600 text-2xl mb-5">{skillsTitle}</h3>
-      <ul className="flex m-auto justify-center flex-wrap gap-5 max-w-xl">
+      <h3 className='py-4 text-teal-600 text-2xl mb-5'>{skillsTitle}</h3>
+      <ul className='flex m-auto justify-center flex-wrap gap-5 max-w-xl'>
         {skills.map((skill: skill) => (
           <li
             key={skill.id}
             className={`${switchText} ${
-              isPage ? "w-1/4" : "w-full sm:w-1/5"
+              isPage ? 'w-1/4' : 'w-full sm:w-1/5'
             } rounded py-2`}
           >
             <img
-              className={`w-40 sm:w-30 lg:w-40 mx-auto`}
+              className={`w-40 sm:w-30 lg:w-40 mx-auto mix-blend-${
+                darkMode ? 'lighten' : 'darken'
+              }`}
               src={skill.img}
               alt={skill.name}
             />
@@ -38,7 +41,7 @@ const Skills: FC<TSkillsProps> = ({
         ))}
       </ul>
     </div>
-  );
-};
+  )
+}
 
-export default Skills;
+export default Skills
